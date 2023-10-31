524c616b8d66c1b0e5bbb415a4e0bd8fc9cdfdfa
Tom Reinhardt <tom.reinhardt@check24.de>
2023-10-31 18:04:04 +0100

    Initial Commit

---
+++ SwiftDataKnowledgeSharing/SwiftDataKnowledgeSharingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataKnowledgeSharingApp.swift
+//  SwiftDataKnowledgeSharing
+//
+//  Created by Tom Reinhardt on 31.10.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataKnowledgeSharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
